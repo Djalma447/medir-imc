794d9934810dcf9cd4a8875ddf672aa908b8881c
Djalma <djalma.ant@gmail.com>
2019-11-26 02:20:25 -0300

    Inicio do Projeto, Titulo Inicial do Programa

---
+++ MedidorDeIMC/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MedidorDeIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa Medidor de IMC (Índice de Massa Corporal)");
+
+        }
+    }
+}
